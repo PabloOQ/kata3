02b3417880336fdaf190c4ef171cc9d928ccbe09
Pablo Ortigosa <55493443+PabloOQ@users.noreply.github.com>
2020-11-04 09:22:42 +0000

    Add files via upload

---
+++ src/kata3/Kata3.java
@@ -0,0 +1,18 @@
+package kata3;
+
+/**
+ *
+ * @author Pablo
+ */
+public class Kata3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        HistogramDisplay histo = new HistogramDisplay("HISTOGRAM...");
+        
+        histo.execute();
+    }
+    
+}
